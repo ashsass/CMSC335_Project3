ff6889c85ff5a23b7f1e0ba3405255efa8499ab4
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-05-01 10:02:36 -0400

    Set up basic code for primaryStage and added images.

--- CMSC335_Project3/src/application/TrafficGUI.java
+++ CMSC335_Project3/src/application/TrafficGUI.java
@@ -1,5 +1,16 @@
 package application;
 
-public class TrafficGUI {
+import javafx.scene.layout.*;
+
+public class TrafficGUI extends BorderPane {
+	// Different portions need to make:
+		// left border for display pane
+		// bottom border for play, pause, etc buttons
+		// center border to hold the main ui
+	
+	public void createDisplayPanel() {
+		
+	}
+	
 
 }
--- CMSC335_Project3/src/application/TrafficMain.java
+++ CMSC335_Project3/src/application/TrafficMain.java
@@ -3,7 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 
 
 public class TrafficMain extends Application {
@@ -11,9 +11,10 @@ public class TrafficMain extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(root,600,300);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Traffic Control");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
