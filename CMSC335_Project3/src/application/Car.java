f0425c4a6e5c7ad04fa01be412f337cbedbd8dc5
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-05-04 16:33:19 -0400

    Can add a new car.

--- CMSC335_Project3/src/application/Car.java
+++ CMSC335_Project3/src/application/Car.java
@@ -3,9 +3,15 @@ package application;
 import javafx.scene.image.*;
 
 public class Car {
-	ImageView car;
+	private ImageView car;
 	
 	public Car() {
 		car = new ImageView(new Image(getClass().getResourceAsStream("/resources/car.png")));
+		car.setFitWidth(30);
+		car.setFitHeight(39);
+	}
+	
+	public ImageView getCar() {
+		return car;
 	}
 }
--- CMSC335_Project3/src/application/Controller.java
+++ CMSC335_Project3/src/application/Controller.java
@@ -1,5 +1,15 @@
 package application;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
 
+public class Controller {
+	
+	@FXML
+	private GridPane gridPane;
+	
+	public void addCar() {
+		Car car = new Car();
+		gridPane.add(car.getCar(), 0, 2);
+	}
 }
