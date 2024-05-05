9ee611fa038454f92882a108eae92ce89c71782c
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-05-04 20:21:21 -0400

    Moved width and height to method to be called in Light

--- CMSC335_Project3/src/application/Light.java
+++ CMSC335_Project3/src/application/Light.java
@@ -5,15 +5,12 @@ import javafx.scene.image.ImageView;
 
 public class Light {
 	private ImageView light;
-	private final Double Y_COORD = 139.0;
+	private final Double Y_COORD = 119.0;
 	static int id = 0;
 	
 	public Light() {
 		light = new ImageView(new Image(getClass().getResourceAsStream("/resources/green-light.png")));
-		light.setFitWidth(34);
-		light.setFitHeight(41);
-//		light.setLayoutX(0);
-//		light.setLayoutY(175);
+		setSize();
 		id++;
 	}
 	
@@ -24,5 +21,10 @@ public class Light {
 	public void setPlacement(Double x) {
 		light.setLayoutX(x);
 		light.setLayoutY(Y_COORD);
+	}
+	
+	private void setSize() {
+		light.setFitWidth(24);
+		light.setFitHeight(31);
 	}
 }
