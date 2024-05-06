f75347371751402d7d22f4ef46753ab3e914f2d2
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-05-06 15:09:33 -0400

    Methods created to change the light image.

--- CMSC335_Project3/src/application/Light.java
+++ CMSC335_Project3/src/application/Light.java
@@ -2,14 +2,26 @@ package application;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import java.util.Random;
 
 public class Light {
 	private ImageView light;
 	private final Double Y_COORD = 119.0;
-	static int id = 0;
+	private static int id = 0;
+	private String[] lightImages = {"/resources/red-light.png", "/resources/yellow-light.png", "/resources/green-light.png"};
+	/* lightImages index:
+	 	* [0] = red light
+	 	* [1] = yellow light
+	 	* [2] = green light
+	 */
+	private int imageIndex = initializeLight();
 	
+	// Light's ImageView set using the initializeLight to randomly pull a light image from the array
 	public Light() {
-		light = new ImageView(new Image(getClass().getResourceAsStream("/resources/green-light.png")));
+		light = new ImageView(
+				new Image(
+						getClass()
+						.getResourceAsStream(lightImages[imageIndex])));
 		setSize();
 		id++;
 	}
@@ -24,8 +36,33 @@ public class Light {
 	}
 	// this maybe should be modified so it can't be accessed or changed? 
 	
-	private void setSize() {
+	private final void setSize() {
 		light.setFitWidth(24);
 		light.setFitHeight(31);
+	}
+	
+	// Use this int to pull a random image from the light array
+	// Instead of calling the method in the constructor I'm going to assign it to a variable
+	// then I can use the global variable to move through the other images 
+	public int initializeLight() {
+		return new Random().nextInt(3); // 0-2 inclusive, 3 exclusive
+	}
+	
+	// Want the light to move through different images every few seconds
+	// Go from green -> yellow -> red
+		// If the light starts on red -> green -> yellow -> red
+		// So on an so forth
+		// Maybe make it recursive?
+	public ImageView setNewLight() {
+		imageIndex++;
+		if(imageIndex == 3) {
+			imageIndex = 0; // want to reset this
+			// but makes the class even further mutable maybe want to figure out a safer way to do this
+			// maybe call a new object - Light(new ImageView?)
+		}
+		return new ImageView(
+				new Image(
+						getClass()
+						.getResourceAsStream(lightImages[imageIndex])));
 	}
 }
